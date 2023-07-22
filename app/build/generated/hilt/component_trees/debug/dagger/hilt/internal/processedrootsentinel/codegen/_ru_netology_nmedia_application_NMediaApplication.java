7b3a8897cc3f1dfe9ccd391855b1b1cf5f504164
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2023-07-22 12:52:00 +0700

    Удалены игнорируемые файлы

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_ru_netology_nmedia_application_NMediaApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "ru.netology.nmedia.application.NMediaApplication"
-)
-public final class _ru_netology_nmedia_application_NMediaApplication {
-}
